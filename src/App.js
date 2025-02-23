2a4b392e6d733cd00205276204740afa923df944
Aviv Shemesh <avivs@sqreamtech.com>
2025-02-23 17:52:18 +0200

    inititating frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/")
+            .then(response => setMessage(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            <h1>React + Node.js</h1>
+            <p>Backend says: {message}</p>
+        </div>
+    );
 }
 
 export default App;
